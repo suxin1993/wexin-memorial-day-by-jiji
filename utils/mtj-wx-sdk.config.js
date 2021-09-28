2b80dff5a9ee8c58b53a44cd0744ac191afedac1
subin <18565641627@163.com>
2021-09-28 20:41:34 +0800

    :zap:[feat]: 添加百度统计以及qq地图

---
+++ utils/mtj-wx-sdk.config.js
@@ -0,0 +1,37 @@
+/*
+ * @Author: your name
+ * @Date: 2021-08-11 15:43:54
+ * @LastEditTime: 2021-09-28 14:06:30
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: /wexin-memorial-day-by-jiji/utils/mtj-wx-sdk.config.js
+ */
+/**
+ * @file 百度移动统计配置文件
+ */
+
+module.exports = {
+    /**
+     * 从百度移动统计获取的AppKey
+     * @type {string}
+     */
+    appKey: '5eed97a387',
+
+    /**
+     * 是否使用了插件
+     * @type {boolean}
+     */
+    hasPlugin: false,
+
+    /**
+     * 是否获取当前的地理位置和速度信息
+     * @type {boolean}
+     */
+    getLocation: false,
+
+    /**
+     * 是否获取组件滚动信息
+     * @type {boolean}
+     */
+    getComponentScroll: false,
+};
