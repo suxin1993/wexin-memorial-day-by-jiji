e06f957bbd0f2d46e0b8c48e4b2d17ee96869301
suxin <15626659532@.163com>
2021-09-29 01:50:15 +0800

    :zap: [fix]: 更新获取userInfo的方法

--- app.js
+++ app.js
@@ -14,12 +14,16 @@ App({
       //调用登录接口
       wx.login({
         success: function () {
-          wx.getUserInfo({
-            success: function (res) {
-              that.globalData.userInfo = res.userInfo
-              typeof cb == "function" && cb(that.globalData.userInfo)
-            }
-          })
+         // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认
+      // 开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
+      wx.getUserProfile({
+        desc: '用于完善会员资料', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+        success: (res) => {
+          that.globalData.userInfo = res.userInfo
+          typeof cb == "function" && cb(that.globalData.userInfo)
+          // 缓存到store中
+        }
+      })
         }
       })
     }
