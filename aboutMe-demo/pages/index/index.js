eb175ee0b3aacd9fa5f7d9e52df3bfad587d101e
subin <18565641627@163.com>
2022-01-28 18:19:00 +0800

    [fix]:  添加demo

---
+++ aboutMe-demo/pages/index/index.js
@@ -0,0 +1,93 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    poster: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000',
+    audioPoster: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000',
+    audioName: '此时此刻',
+    audioAuthor: '许巍',
+    audioSrc: 'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E06DCBDC9AB7C49FD713D632D313AC4858BACB8DDD29067D3C601481D36E62053BF8DFEAF74C0A5CCFADD6471160CAF3E6A&fromtag=46',
+  
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  onReady: function (e) {
+    // 使用 wx.createAudioContext 获取 audio 上下文 context
+    this.audioCtx = wx.createAudioContext('myAudio')
+  },
+
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
+  //播放音乐的函数
+  audioPlay: function () {
+    this.audioCtx.play()
+  },
+  audioPause: function () {
+    this.audioCtx.pause()
+  },
+  audio14: function () {
+    this.audioCtx.seek(14)
+  },
+  audioStart: function () {
+    this.audioCtx.seek(0)
+  },
+
+  //调用点击按钮，复制博客
+  copytoCliboard(){
+    wx.setClipboardData({
+      data: 'https://blog.csdn.net/weixin_42061487',
+      success (res) {
+        wx.getClipboardData({
+          success (res) {
+            console.log(res.data) // data
+          }
+        })
+      }
+    })
+  }
+
+})
---
+++ aboutMe-demo/pages/photos/photos.js
@@ -0,0 +1,69 @@
+//index.js
+//获取应用实例
+var app = getApp();
+Page({
+  data: {
+    animationData: {},
+    cardInfoList: [{
+      cardUrl: '../../images/bgimages/mj1.jpg',
+      cardInfo: {
+        cardTitle: '黑暗总会过去',
+        cardInfoMes: ['没有压力的生活就会空虚；', '没有压力的青春就会枯萎；', '没有压力的生命就会黯淡。']
+      }
+    }, {
+        cardUrl: '../../images/bgimages/mj2.jpg',
+      cardInfo: {
+        cardTitle: '黎明终会到来。',
+        cardInfoMes: ['立足今日', '展望明天', '每天都是一个新的起点']
+      }
+    }, {
+      cardUrl: '../../images/bgimages/mj3.jpg',
+      cardInfo: {
+        cardTitle: '你从不孤单',
+        cardInfoMes: ['随性而行，无需刻意；', '随遇而安，切勿奢望;', '随心而静，不要烦忧。']
+      }
+    }]
+  },
+  //事件处理函数
+  slidethis: function (e) {
+    console.log(e);
+    var animation = wx.createAnimation({
+      duration: 300,
+      timingFunction: 'cubic-bezier(.8,.2,.1,0.8)',
+    });
+    var self = this;
+    this.animation = animation;
+    this.animation.translateY(-420).rotate(-5).translateX(0).step();
+    this.animation.translateY(62).translateX(25).rotate(0).step();
+    this.setData({
+      animationData: this.animation.export()
+    });
+    setTimeout(function () {
+      var cardInfoList = self.data.cardInfoList;
+      var slidethis = self.data.cardInfoList.shift();
+      self.data.cardInfoList.push(slidethis);
+      self.setData({
+        cardInfoList: self.data.cardInfoList,
+        animationData: {}
+      });
+    }, 350);
+  },
+  buythis: function (e) {
+    console.log(e);
+    app.buyDetail = this.data.cardInfoList[e.target.id];
+    wx.navigateTo({
+      url: '../about/about'
+    });
+  },
+  onLoad: function () {
+    console.log('onLoad');
+    var that = this;
+    //调用应用实例的方法获取全局数据
+    // app.getUserInfo(function (userInfo) {
+    //   //更新数据
+    //   that.setData({
+    //     userInfo: userInfo
+    //   });
+    // });
+  }
+})
